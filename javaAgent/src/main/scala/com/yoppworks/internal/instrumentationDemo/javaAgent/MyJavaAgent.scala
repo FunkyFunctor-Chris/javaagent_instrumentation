3f8fdc1859a3a6080cd169596dc858cd6cad7564
Chris André <chris.andre@yoppworks.com>
2019-01-29 22:47:32 -0500

    Added demonstration of the java agent arguments

--- javaAgent/src/main/scala/com/yoppworks/internal/instrumentationDemo/javaAgent/MyJavaAgent.scala
+++ javaAgent/src/main/scala/com/yoppworks/internal/instrumentationDemo/javaAgent/MyJavaAgent.scala
@@ -3,9 +3,9 @@ package com.yoppworks.internal.instrumentationDemo.javaAgent
 import java.lang.instrument.Instrumentation
 
 object MyJavaAgent {
-  def premain(
-    agentArgs: String, inst: Instrumentation ): Unit = {
+  def premain(agentArgs: String, inst: Instrumentation): Unit = {
     System.err.println(">>> Loading up java agent!")
+    System.err.println(s">>> Parameters passed to the java agent: '$agentArgs'")
     inst.addTransformer(new Transformer())
   }
 }
